2af15e4fa2f36ad64e8cfac2b93aff708b9c93e1
Xilv3r@gmail.com <xilv3r@gmail.com>
2012-06-21 19:46:48 +0000

    Haroid 1.5-SNAPSHOT: Optimized preference pre summary setter code

--- haroid/src/main/java/nl/haroid/SettingsActivity.java
+++ haroid/src/main/java/nl/haroid/SettingsActivity.java
@@ -3,9 +3,11 @@ package nl.haroid;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Ruud de Jong
@@ -17,29 +19,27 @@ public final class SettingsActivity extends PreferenceActivity implements OnShar
     public static final String TEGOED_PREF_KEY = "pref_max_tegoed";
     public static final String STARTDAG_PREF_KEY = "pref_start_tegoed";
 
-    private EditTextPreference usernameText;
-    private EditTextPreference tegoedText;
-    private ListPreference startdagList;
-
+    private Map<String, String> pretextMap = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-        //Get instance of specific preferance
-        usernameText = (EditTextPreference)getPreferenceScreen().findPreference(USERNAME_PREF_KEY);
-        tegoedText = (EditTextPreference)getPreferenceScreen().findPreference(TEGOED_PREF_KEY);
-        startdagList = (ListPreference)getPreferenceScreen().findPreference(STARTDAG_PREF_KEY);
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //Set initial values
-        usernameText.setSummary(getPreferenceScreen().getSharedPreferences().getString(USERNAME_PREF_KEY, getString(R.string.change_emailadres_sum)));
-        tegoedText.setSummary(getString(R.string.tegoed_setting_pretext) + " " + getPreferenceScreen().getSharedPreferences().getString(TEGOED_PREF_KEY, "0"));
-        startdagList.setSummary(getString(R.string.startdag_setting_pretext) + " " + getPreferenceScreen().getSharedPreferences().getString(STARTDAG_PREF_KEY, "0"));
+        //Load map
+        pretextMap.put(USERNAME_PREF_KEY, getString(R.string.emailadres_setting_pretext));
+        pretextMap.put(TEGOED_PREF_KEY, getString(R.string.tegoed_setting_pretext));
+        pretextMap.put(STARTDAG_PREF_KEY, getString(R.string.startdag_setting_pretext));
+
+        //Iterate over all preferance objects and set initial values
+        for (Map.Entry<String, String> entry : pretextMap.entrySet()){
+            changePreferanceSummary(entry.getKey(), getPreferenceScreen().getSharedPreferences());
+        }
+
         //Reregister Listener due to application being resumed
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
@@ -53,13 +53,12 @@ public final class SettingsActivity extends PreferenceActivity implements OnShar
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        //todo (Dave) get rid of if else flow
-        if (s.equals(USERNAME_PREF_KEY)){
-            usernameText.setSummary(getString(R.string.emailadres_setting_pretext) + " " + sharedPreferences.getString(s, ""));
-        } else if (s.equals(TEGOED_PREF_KEY)){
-            tegoedText.setSummary(getString(R.string.tegoed_setting_pretext) + " " + sharedPreferences.getString(s, ""));
-        } else if (s.endsWith(STARTDAG_PREF_KEY)){
-            startdagList.setSummary(getString(R.string.startdag_setting_pretext) + " " + sharedPreferences.getString(s, ""));
-        }
+        changePreferanceSummary(s, sharedPreferences);
     }
+
+    private void changePreferanceSummary(String s, SharedPreferences sharedPreferences) {
+        Preference preference = getPreferenceScreen().findPreference(s);
+        preference.setSummary(pretextMap.get(s) + " " + sharedPreferences.getString(s, ""));
+    }
+
 }
