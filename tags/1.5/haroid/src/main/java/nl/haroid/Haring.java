40bee373bc46e73e547218a1b78f9091ae9f9804
biker.ruud@gmail.com <biker.ruud@gmail.com@f512c360-b46e-778e-c973-32242b8f1c5d>
2012-06-23 07:18:44 +0000

    Tagging 1.5 with history deletion button.

---
+++ tags/1.5/haroid/src/main/java/nl/haroid/Haring.java
@@ -0,0 +1,314 @@
+package nl.haroid;
+
+import android.net.http.AndroidHttpClient;
+import android.util.Log;
+import org.apache.http.*;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ruud de Jong
+ */
+public final class Haring {
+    private static final String LOG_TAG = "Haring";
+
+    private String host = "www.hollandsnieuwe.nl";
+    private int port = 443;
+    private String schemeName = "https";
+    private String relativeUrlStart = "/mijn_hollandsnieuwe";
+    private String relativeUrlVerbruik = "/myaccount/subscriptionPurchaseCurrentUsageDataFeed.jsp?index=0&status=ACTIVE&profileId=";
+    private String username;
+    private String password;
+
+
+    public String start(String username, String password) {
+        this.username = username;
+        this.password = password;
+        AndroidHttpClient haringClient = getHaringClient();
+        HttpContext localContext = getHaringContext();
+        try {
+            boolean ingelogd = login(haringClient, localContext);
+            if (ingelogd) {
+                String tegoed = haalVerbruikGegevensOp(haringClient, localContext);
+                Log.i(LOG_TAG, "Gevonden tegoed: " + tegoed);
+                return tegoed;
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Kaput", e);
+        } catch (URISyntaxException e) {
+            Log.e(LOG_TAG, "Kaput", e);
+        } finally {
+            haringClient.getConnectionManager().shutdown();
+            haringClient.close();
+        }
+        return "Helaas niet gevonden.";
+    }
+
+    private String haalVerbruikGegevensOp(HttpClient haringClient, HttpContext localContext) throws IOException {
+        String tegoedIndicator = "minuten, smsjes of MB";
+        Log.d(LOG_TAG, "**********************************");
+        Log.d(LOG_TAG, "**        USAGE                 **");
+        Log.d(LOG_TAG, "**********************************");
+        HttpGet verbruikGet = new HttpGet(relativeUrlVerbruik);
+        HttpResponse response = haringClient.execute(verbruikGet, localContext);
+//        logResponseStatus(response);
+//        logCookieStore(localContext);
+        HttpEntity responseEntity = response.getEntity();
+        if (responseEntity == null) {
+            return null;
+        }
+        InputStream inputStream = responseEntity.getContent();
+        String body = Utils.toString(inputStream);
+        //LOG.info(body);
+        String tegoedBedrag = Utils.substringBetween(body, "<span class=\"usage\"><span class=\"amount\">", "</span>");
+        String[] strongList = Utils.substringsBetween(body, "<strong>", "</strong>");
+        String tegoed = null;
+        if (strongList != null) {
+            for (String strongItem : strongList) {
+                if (Utils.contains(strongItem, tegoedIndicator)) {
+                    String filterItem = Utils.deleteWhitespace(strongItem);
+                    tegoed = Utils.remove(filterItem, Utils.deleteWhitespace(tegoedIndicator));
+                }
+            }
+        }
+        Log.d(LOG_TAG, "Gevonden tegoed bedrag: " + tegoedBedrag);
+        Log.d(LOG_TAG, "Gevonden tegoed: " + tegoed);
+        return tegoed;
+    }
+
+    private boolean login(HttpClient haringClient, HttpContext localContext) throws IOException, URISyntaxException {
+        Log.d(LOG_TAG, "**********************************");
+        Log.d(LOG_TAG, "**        LOGIN                 **");
+        Log.d(LOG_TAG, "**********************************");
+        HttpGet getLogin = new HttpGet(relativeUrlStart);
+        HttpResponse response = haringClient.execute(getLogin, localContext);
+//        logResponseStatus(response);
+//        logCookieStore(localContext);
+
+        HttpEntity entity = response.getEntity();
+        if (entity == null) {
+            Log.i(LOG_TAG, "Kan het inlogscherm niet ophalen");
+            return false;
+        }
+        InputStream inputStream = entity.getContent();
+        String body = Utils.toString(inputStream);
+
+        Form form = parseForm(body);
+
+        if (form == null) {
+            Log.i(LOG_TAG, "Kan het inlogscherm niet vinden");
+            Log.i(LOG_TAG, body);
+            return false;
+        }
+        Log.i(LOG_TAG, "Form:");
+        Log.i(LOG_TAG, "Form action: " + form.action);
+        Log.i(LOG_TAG, "Form inputs: " + form.inputList.size());
+//        for (InputType type : InputType.values()) {
+//            Log.i(LOG_TAG, "Form " + type.toString() + " inputs:");
+//            for (Input input : form.inputList) {
+//                if (input.getType() == type) {
+//                    Log.i(LOG_TAG, " - " + input.getName() + " = " + input.getValue());
+//                }
+//            }
+//        }
+        Input loginInput = getLoginInput(form);
+        Input passwordInput = getPasswordInput(form);
+        if (loginInput != null && passwordInput != null) {
+            loginInput.value = username;
+            passwordInput.value = password;
+            postForm(haringClient, localContext, form);
+            return true;
+        } else {
+            Log.i(LOG_TAG, "Kan kan login en password velden vinden op het login scherm.");
+            return false;
+        }
+    }
+
+    private Input getLoginInput(Form form) {
+        if (form == null || form.inputList == null) {
+            return null;
+        }
+        for (Input input : form.inputList) {
+            if (input.getType() == InputType.TEXT) {
+                if (Utils.containsIgnoreCase(input.getName(), "login")) {
+                    Log.i(LOG_TAG, "Possible login input: " + input.getName());
+                    return input;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Input getPasswordInput(Form form) {
+        if (form == null || form.inputList == null) {
+            return null;
+        }
+        for (Input input : form.inputList) {
+            if (input.getType() == InputType.PASSWORD) {
+                if (Utils.containsIgnoreCase(input.getName(), "password")) {
+                    Log.i(LOG_TAG, "Possible password input: " + input.getName());
+                    return input;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void postForm(HttpClient haringClient, HttpContext localContext, Form form) throws IOException, URISyntaxException {
+        Log.i(LOG_TAG, "Raw form action: " + form.action);
+        HttpRequest requestSent = (HttpRequest) localContext.getAttribute(ExecutionContext.HTTP_REQUEST);
+        URI previousRequestUri = new URI(requestSent.getRequestLine().getUri());
+        URI resolvedFormAction = previousRequestUri.resolve(form.action);
+        Log.i(LOG_TAG, "Posting form to: " + resolvedFormAction.toString());
+
+        HttpPost post = new HttpPost(resolvedFormAction);
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+        for (Input input : form.inputList) {
+            formparams.add(new BasicNameValuePair(input.getName(), input.getValue()));
+        }
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+        post.setEntity(entity);
+        HttpResponse response = haringClient.execute(post, localContext);
+//        logResponseStatus(response);
+//        logCookieStore(localContext);
+//        HttpEntity responseEntity = response.getEntity();
+//        if (responseEntity != null) {
+//            InputStream inputStream = responseEntity.getContent();
+//            String body = Utils.toString(inputStream);
+//        }
+    }
+
+    private Form parseForm(String body) {
+        String entireForm = Utils.substringBetween(body, "<form ", "</form>");
+        String formHeader = Utils.substringBetween(body, "<form ", ">");
+        if (entireForm == null || formHeader == null) {
+            return null;
+        }
+        Form response = new Form();
+        String formAction = Utils.substringBetween(formHeader, "action=\"", "\"");
+        response.action = formAction;
+        response.inputList = parseInputs(entireForm);
+        return response;
+    }
+
+    private List<Input> parseInputs(String entireForm) {
+        String[] inputStringArray = Utils.substringsBetween(entireForm, "<input ", ">");
+        List<Input> inputs = new ArrayList<Input>();
+        for (String inputString : inputStringArray) {
+            String name = Utils.substringBetween(inputString, "name=\"", "\"");
+            String value = Utils.substringBetween(inputString, "value=\"", "\"");
+            String type = Utils.substringBetween(inputString, "type=\"", "\"");
+            InputType inputType = InputType.UNKNOWN;
+            if (Utils.equalsIgnoreCase(type, InputType.HIDDEN.toString())) {
+                inputType = InputType.HIDDEN;
+            } else if (Utils.equalsIgnoreCase(type, InputType.TEXT.toString())) {
+                inputType = InputType.TEXT;
+            } else if (Utils.equalsIgnoreCase(type, InputType.SUBMIT.toString())) {
+                inputType = InputType.SUBMIT;
+            } else if (Utils.equalsIgnoreCase(type, InputType.PASSWORD.toString())) {
+                inputType = InputType.PASSWORD;
+            } else if (Utils.equalsIgnoreCase(type, InputType.CHECKBOX.toString())) {
+                inputType = InputType.CHECKBOX;
+            }
+
+            if (name != null && value != null) {
+                inputs.add(new Input(name, value, inputType));
+            }
+        }
+        return inputs;
+    }
+
+//    private void logResponseStatus(HttpResponse response) {
+//        Log.i(LOG_TAG, "Status code: " + response.getStatusLine().getStatusCode());
+//        Header[] headers = response.getAllHeaders();
+//        if (headers != null) {
+//            Log.i(LOG_TAG, "Headers:");
+//            for (Header header : headers) {
+//                Log.i(LOG_TAG, " - " + header.getName() + " = " + header.getValue());
+//            }
+//        }
+//    }
+//
+//    private void logCookieStore(HttpContext haringContext) {
+//        CookieStore cookieStore = (CookieStore) haringContext.getAttribute(ClientContext.COOKIE_STORE);
+//        List<Cookie> cookieList = cookieStore.getCookies();
+//        if (cookieList != null && cookieList.size() > 0) {
+//            Log.i(LOG_TAG, "Cookies:");
+//            for (Cookie cookie : cookieList) {
+//                Log.i(LOG_TAG, " - " + cookie.getName() + " = " + cookie.getValue());
+//            }
+//        }
+//    }
+
+    private AndroidHttpClient getHaringClient() {
+        String userAgent = "Mozilla/5.0(Linux; U; Android 2.2; en-gb)";
+        AndroidHttpClient haringClient = AndroidHttpClient.newInstance(userAgent);
+        //((AndroidHttpClient) haringClient).setRedirectStrategy(new LaxRedirectStrategy());
+
+        // Setup redirects
+        haringClient.getParams().setParameter(ClientPNames.DEFAULT_HOST, new HttpHost(host, port, schemeName));
+        haringClient.getParams().setBooleanParameter(ClientPNames.HANDLE_REDIRECTS, Boolean.TRUE);
+
+        return haringClient;
+    }
+
+    private HttpContext getHaringContext() {
+        // Setup cookie store
+        CookieStore cookieStore = new BasicCookieStore();
+        HttpContext localContext = new BasicHttpContext();
+        localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+        return localContext;
+    }
+
+    enum InputType {
+        TEXT, HIDDEN, SUBMIT, PASSWORD, CHECKBOX, UNKNOWN
+    }
+
+    private class Form {
+        String action;
+        List<Input> inputList;
+    }
+
+    private class Input {
+        String name;
+        String value;
+        InputType type;
+
+        Input(String name, String value, InputType type) {
+            this.name = name;
+            this.value = value;
+            this.type = type;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+        public InputType getType() {
+            return this.type;
+        }
+    }
+
+}
---
+++ tags/1.5/haroid/src/main/java/nl/haroid/Haroid.java
@@ -0,0 +1,291 @@
+package nl.haroid;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+public final class Haroid extends Activity implements TegoedConsumer {
+    private static final String LOG_TAG = "Haroid";
+    private static final String CURRENT_TEGOED = "current tegoed";
+
+    private HistoryMonitor historyMonitor;
+
+    private int currentTegoed = -1;
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(LOG_TAG, "onCreate");
+
+        setContentView(R.layout.main);
+        initControls();
+        if (savedInstanceState != null) {
+            Log.i(LOG_TAG, "onCreate has savedInstanceState");
+            this.currentTegoed = savedInstanceState.getInt(CURRENT_TEGOED, -1);
+            Log.i(LOG_TAG, "onCreate tegoed: " + this.currentTegoed);
+            if (this.currentTegoed != -1) {
+                setTegoedProgress(this.currentTegoed);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (this.currentTegoed != -1) {
+            outState.putInt(CURRENT_TEGOED, currentTegoed);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int maxTegoed = Integer.parseInt(prefs.getString("pref_max_tegoed", "0"));
+        int startTegoed = Integer.parseInt(prefs.getString("pref_start_tegoed", "0"));
+        bepaalGeschiedenis(startTegoed);
+        int maxPeriod = berekenDuurTegoed(startTegoed);
+        tekenVerbruik(maxTegoed, maxPeriod);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(LOG_TAG, "onCreateOptionsMenu");
+        menu.add(Menu.NONE, R.id.menuSettings, 0, getString(R.string.settings));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(LOG_TAG, "onOptionsItemSelected");
+        switch (item.getItemId()) {
+            case R.id.menuSettings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                Log.i(LOG_TAG, "Verkeerde optie");
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void initControls() {
+        this.historyMonitor = new HistoryMonitor(this);
+        Button tegoedButton = (Button) findViewById(R.id.ButtonTegoed);
+        Button removeHistoryButton = (Button) findViewById(R.id.ButtonRemoveHistory);
+
+        tegoedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startHaring();
+            }
+        });
+
+        removeHistoryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                confirmRemoveHistory();
+            }
+        });
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int startTegoed = Integer.parseInt(prefs.getString("pref_start_tegoed", "0"));
+        berekenDuurTegoed(startTegoed);
+    }
+
+    private void startHaring() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String emailAdres = prefs.getString("pref_username", "");
+        String wachtwoord = prefs.getString("pref_password", "");
+
+        if (emailAdres != null && emailAdres.length() > 0 && wachtwoord != null && wachtwoord.length() > 0) {
+            TextView tegoedView = (TextView) findViewById(R.id.TextTegoed);
+            tegoedView.setText(getString(R.string.periodeTegoed) + " wordt opgehaald.");
+            HaringTask haringTask = new HaringTask();
+            haringTask.setTegoedConsumer(this);
+            haringTask.execute(emailAdres, wachtwoord);
+        }
+    }
+
+    private void tekenVerbruik(int maxTegoed, int maxPeriod) {
+        Log.i(LOG_TAG, "tekenVerbruik");
+        if (maxTegoed > 0 && maxPeriod > 0) {
+            List<HistoryMonitor.UsagePoint> usageList = this.historyMonitor.getUsageList();
+            MonthlyGraphView verbruikGraph = (MonthlyGraphView) findViewById(R.id.MonthlyGraphVerbruik);
+            verbruikGraph.setMaxPeriod(maxPeriod);
+            verbruikGraph.setMaxUnits(maxTegoed);
+            verbruikGraph.setUsage(usageList);
+            verbruikGraph.invalidate();
+            DailyGraphView dagelijksVerbruikGraph = (DailyGraphView) findViewById(R.id.DailyGraphVerbruik);
+            dagelijksVerbruikGraph.setMaxPeriod(maxPeriod);
+            dagelijksVerbruikGraph.setMaxUnits(maxTegoed);
+            dagelijksVerbruikGraph.setUsage(usageList);
+            dagelijksVerbruikGraph.invalidate();
+        }
+    }
+
+    private int berekenDuurTegoed(int startTegoed) {
+        Log.i(LOG_TAG, "berekenDuurTegoed");
+        ProgressBar duurTegoedProgress = (ProgressBar) findViewById(R.id.PbarTijd);
+        if (startTegoed == 0) {
+            // Niet ingesteld.
+            duurTegoedProgress.setProgress(0);
+            return 0;
+        }
+        int maxPeriod = berekenMaxPeriod(startTegoed);
+        Calendar cal = Calendar.getInstance();
+        int huidigeDagVdMaand = cal.get(Calendar.DAY_OF_MONTH);
+        int verbruik = 0;
+        if (huidigeDagVdMaand < startTegoed) {
+            // Tegoed begonnen in vorige maand
+            Log.i(LOG_TAG, "Tegoed begonnen in vorige maand");
+            verbruik = (maxPeriod - startTegoed) + 1;
+            verbruik += huidigeDagVdMaand;
+        } else {
+            // Tegoed begonnen in deze maand.
+            Log.i(LOG_TAG, "Tegoed begonnen in deze maand");
+            verbruik = (huidigeDagVdMaand - startTegoed) + 1;
+        }
+        int nogTeGaan = maxPeriod - verbruik;
+        int currentPeriod = verbruik;
+        Log.i(LOG_TAG, "currentPeriod: " + currentPeriod);
+        Log.i(LOG_TAG, "maxPeriod: " + maxPeriod);
+        duurTegoedProgress.setMax(maxPeriod);
+        duurTegoedProgress.setProgress(nogTeGaan);
+        TextView tijdView = (TextView) findViewById(R.id.TextTijd);
+        if (nogTeGaan == 1) {
+            tijdView.setText(getString(R.string.duurPeriode) + " nog " + nogTeGaan + " dag te gaan.");
+        } else if (nogTeGaan < 1) {
+            tijdView.setText(getString(R.string.duurPeriode) + " laatste dag.");
+        } else {
+            tijdView.setText(getString(R.string.duurPeriode) + " nog " + nogTeGaan + " dagen te gaan.");
+        }
+        return maxPeriod;
+    }
+
+    private int berekenMaxPeriod(int startTegoed) {
+        Calendar cal = Calendar.getInstance();
+        int huidigeDagVdMaand = cal.get(Calendar.DAY_OF_MONTH);
+        if (huidigeDagVdMaand < startTegoed) {
+            // Tegoed begonnen in vorige maand
+            Log.i(LOG_TAG, "Tegoed begonnen in vorige maand");
+            return Utils.numberOfDaysPreviousMonth();
+        } else {
+            // Tegoed begonnen in deze maand.
+            Log.i(LOG_TAG, "Tegoed begonnen in deze maand");
+            return Utils.numberOfDaysThisMonth();
+        }
+    }
+
+    private void bepaalGeschiedenis(int startTegoed) {
+        if (startTegoed == 0) {
+            // Niet ingesteld
+            return;
+        }
+        int periodeNummer = Utils.bepaalPeriodeNummer(startTegoed);
+        Log.i(LOG_TAG, "periodeNummer: " + periodeNummer);
+        this.historyMonitor.setPeriodeNummer(periodeNummer);
+    }
+
+    @Override
+    public void setTegoed(int tegoed) {
+        this.currentTegoed = tegoed;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int maxTegoed = Integer.parseInt(prefs.getString("pref_max_tegoed", "0"));
+        int startTegoed = Integer.parseInt(prefs.getString("pref_start_tegoed", "0"));
+        if (tegoed >= 0 && maxTegoed > 0) {
+            int dagInPeriode = Utils.bepaaldDagInPeriode(startTegoed);
+            Log.i(LOG_TAG, "dagInPeriode: " + dagInPeriode);
+            this.historyMonitor.setTegoed(dagInPeriode, tegoed);
+            setTegoedProgress(tegoed);
+            List<HistoryMonitor.UsagePoint> usageList = this.historyMonitor.getUsageList();
+            MonthlyGraphView verbruikGraph = (MonthlyGraphView) findViewById(R.id.MonthlyGraphVerbruik);
+            verbruikGraph.setUsage(usageList);
+            verbruikGraph.invalidate();
+            DailyGraphView dagelijksVerbruikGraph = (DailyGraphView) findViewById(R.id.DailyGraphVerbruik);
+            dagelijksVerbruikGraph.setUsage(usageList);
+            dagelijksVerbruikGraph.invalidate();
+        }
+    }
+
+    private void setTegoedProgress(int tegoed) {
+        Log.i(LOG_TAG, "setTegoedProgress: " + tegoed);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int startTegoed = Integer.parseInt(prefs.getString("pref_start_tegoed", "0"));
+        int maxTegoed = Integer.parseInt(prefs.getString("pref_max_tegoed", "0"));
+        if (tegoed >= 0 && maxTegoed > 0) {
+            TextView tegoedView = (TextView) findViewById(R.id.TextTegoed);
+            tegoedView.setText(getString(R.string.periodeTegoed) + " " + tegoed + " eenheden.");
+            ProgressBar tegoedProgress = (ProgressBar) findViewById(R.id.PbarTegoed);
+            tegoedProgress.setMax(Math.max(tegoed, maxTegoed));
+            tegoedProgress.setProgress(tegoed);
+        }
+        int tegoedGisteren = this.historyMonitor.getTegoedGisteren(maxTegoed);
+        int verbruikVandaag = tegoedGisteren - tegoed;
+        if (tegoedGisteren > -1 && verbruikVandaag >= 0) {
+            int maxPeriod = berekenMaxPeriod(startTegoed);
+            int procentueelVerbruik = (100 * verbruikVandaag * maxPeriod) / maxTegoed;
+            TextView dagVerbruikView = (TextView) findViewById(R.id.TextDagVerbruik);
+            dagVerbruikView.setText(getString(R.string.dagVerbruik) + " " + verbruikVandaag + " eenheden. (" + procentueelVerbruik + "% van de dag)");
+            dagVerbruikView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void confirmRemoveHistory() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.areYouSure))
+                .setCancelable(true)
+                .setPositiveButton(getString(R.string.yesButtonText), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        removeHistory();
+                    }
+                })
+                .setNegativeButton(getString(R.string.noButtonText), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void removeHistory() {
+        this.historyMonitor.resetHistory();
+        this.currentTegoed = 0;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int maxTegoed = Integer.parseInt(prefs.getString("pref_max_tegoed", "0"));
+        TextView tegoedView = (TextView) findViewById(R.id.TextTegoed);
+        tegoedView.setText(getString(R.string.periodeTegoed));
+        ProgressBar tegoedProgress = (ProgressBar) findViewById(R.id.PbarTegoed);
+        tegoedProgress.setMax(maxTegoed);
+        tegoedProgress.setProgress(0);
+        MonthlyGraphView verbruikGraph = (MonthlyGraphView) findViewById(R.id.MonthlyGraphVerbruik);
+        verbruikGraph.setUsage(Collections.<HistoryMonitor.UsagePoint>emptyList());
+        verbruikGraph.invalidate();
+        DailyGraphView dagelijksVerbruikGraph = (DailyGraphView) findViewById(R.id.DailyGraphVerbruik);
+        dagelijksVerbruikGraph.setUsage(Collections.<HistoryMonitor.UsagePoint>emptyList());
+        dagelijksVerbruikGraph.invalidate();
+    }
+}
+
