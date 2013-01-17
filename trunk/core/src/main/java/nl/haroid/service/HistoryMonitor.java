134114527bc3387ee050914e48ab7bcd392b07a5
biker.ruud@gmail.com <biker.ruud@gmail.com@f512c360-b46e-778e-c973-32242b8f1c5d>
2013-01-17 18:56:36 +0000

    History Monitor verplaatst naar core. Mogelijk probleem met jaarwisseling gefixt. TMobile geeft nu 'gebruikersnaam'. 'Wis geschiedenis'-knop is nu naar het menu.

--- trunk/core/src/main/java/nl/haroid/service/HistoryMonitor.java
+++ trunk/core/src/main/java/nl/haroid/service/HistoryMonitor.java
@@ -1,62 +1,47 @@
-package nl.haroid;
+package nl.haroid.service;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
 import nl.haroid.access.BalanceRepository;
 import nl.haroid.common.BundleType;
 import nl.haroid.common.Utils;
-import nl.haroid.storage.StorageOpenHelperAndroidImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.text.DecimalFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Ruud de Jong
  */
 public final class HistoryMonitor {
-    private static final String LOG_TAG = "HistoryMonitor";
+    private static final Logger LOGGER = LoggerFactory.getLogger(HistoryMonitor.class);
 
-    private static final String PERIODE_NUMMER = "pref_periode_nummer";
-    private static final String VERBRUIK_DAG = "pref_verbruik_dag";
-    private final SharedPreferences monitorPrefs;
     private final BalanceRepository balanceRepository;
 
-    public HistoryMonitor(SharedPreferences monitorPrefs, Context context) {
-        this.monitorPrefs = monitorPrefs;
-        this.balanceRepository = new BalanceRepository(new StorageOpenHelperAndroidImpl(context, BalanceRepository.DATABASE_NAME, BalanceRepository.DATABASE_VERSION));
+    public HistoryMonitor(BalanceRepository balanceRepository) {
+        this.balanceRepository = balanceRepository;
     }
 
-    public void convertToDatabase(int startBalance) {
+    public void convertToDatabase(Map<Integer, Integer> prefVerbruikMap, int startBalance) {
         Date lastDayOfPreviousPeriod = Utils.getLastDayOfPreviousPeriod(startBalance);
-        Log.i(LOG_TAG, "convertToDatabase: startBalance=" + startBalance);
-        DecimalFormat decimalFormat = new DecimalFormat("00");
-        for (int i=1; i<32; i++) {
-            String verbruikKey = VERBRUIK_DAG + decimalFormat.format(i);
-            int amountThisDay = this.monitorPrefs.getInt(verbruikKey, -1);
-            if (amountThisDay != -1) {
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(lastDayOfPreviousPeriod);
-                cal.add(Calendar.DATE, i);
-                Log.i(LOG_TAG, "convertToDatabase: daynr " + i + ", amount: " + amountThisDay + ", date: " + cal);
-                balanceRepository.saveOrUpdate(cal.getTime(), amountThisDay, BundleType.MAIN);
-                SharedPreferences.Editor prefEditor = this.monitorPrefs.edit();
-                prefEditor.remove(verbruikKey);
-                prefEditor.commit();
-            }
-        }
-    }
-
-    public void setPeriodeNummer(int periodeNummer) {
-        int huidigePeriodeNummer = this.monitorPrefs.getInt(PERIODE_NUMMER, 0);
-        if (periodeNummer != huidigePeriodeNummer) {
-            resetGeschiedenis(periodeNummer);
+        LOGGER.info("convertToDatabase: startBalance=" + startBalance);
+        for (Map.Entry<Integer, Integer> prefVerbruikItem : prefVerbruikMap.entrySet()) {
+            int dagNummer = prefVerbruikItem.getKey();
+            int dagVerbruik = prefVerbruikItem.getValue();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(lastDayOfPreviousPeriod);
+            cal.add(Calendar.DATE, dagNummer);
+            LOGGER.info("convertToDatabase: daynr " + dagNummer + ", amount: " + dagVerbruik + ", date: " + cal);
+            balanceRepository.saveOrUpdate(cal.getTime(), dagVerbruik, BundleType.MAIN);
         }
     }
 
     public void setTegoed(int dagInPeriode, int tegoed, Date datum) {
         if (dagInPeriode > 0 && dagInPeriode <= 31 && tegoed >= 0) {
-            Log.i(LOG_TAG, "setTegoed " + tegoed + " voor dag " + dagInPeriode);
+            LOGGER.info("setTegoed " + tegoed + " voor dag " + dagInPeriode);
             balanceRepository.saveOrUpdate(datum, tegoed, BundleType.MAIN);
         }
     }
@@ -68,18 +53,18 @@ public final class HistoryMonitor {
         return -1;
     }
 
-    public int getTegoedGisteren(int maxTegoed, int startBalance) {
+    public int getTegoedGisteren(int startBalance) {
         Date lastDayOfPreviousPeriod = Utils.getLastDayOfPreviousPeriod(startBalance);
         return balanceRepository.getMostRecentBalance(lastDayOfPreviousPeriod, new Date(), BundleType.MAIN);
     }
 
     public List<UsagePoint> getUsageList(int startBalance, int maxBalance) {
-        Log.i(LOG_TAG, "getUsageList()");
+        LOGGER.info("getUsageList()");
         Date lastDayOfPreviousPeriod = Utils.getLastDayOfPreviousPeriod(startBalance);
         Map<Integer, Integer> balanceList = balanceRepository.getBalanceList(lastDayOfPreviousPeriod, BundleType.MAIN);
-        Log.i(LOG_TAG, "Balance List");
+        LOGGER.info("Balance List");
         for (Map.Entry<Integer, Integer> balanceListEntry : balanceList.entrySet()) {
-            Log.i(LOG_TAG, balanceListEntry.getKey() + ": " + balanceListEntry.getValue());
+            LOGGER.info(balanceListEntry.getKey() + ": " + balanceListEntry.getValue());
         }
         int amount = maxBalance;
         Calendar cal = Calendar.getInstance();
@@ -88,17 +73,17 @@ public final class HistoryMonitor {
         if (balanceList.get(dateCode) != null) {
             amount = balanceList.get(dateCode);
         }
-        Log.i(LOG_TAG, "max balance of last period: " + amount);
+        LOGGER.info("max balance of last period: " + amount);
         int currentDay = 0;
         List<UsagePoint> verbruikspuntList = new ArrayList<UsagePoint>();
         cal.add(Calendar.DATE, 1); // cal is now first day of period
         for (int i=1; i<32; i++) {
             int amountThisDay = -1;
             dateCode = Utils.bepaalDatumCode(cal.getTime());
-            Log.i(LOG_TAG, "Searching Balance List for: " + dateCode);
+            LOGGER.info("Searching Balance List for: " + dateCode);
             if (balanceList.get(dateCode) != null) {
                 amountThisDay = balanceList.get(dateCode);
-                Log.i(LOG_TAG, "Found it.");
+                LOGGER.info("Found it.");
             }
             if (amountThisDay != -1) {
                 if (amountThisDay <= amount) {
@@ -134,19 +119,11 @@ public final class HistoryMonitor {
     }
 
     public void resetHistory() {
-        Log.i(LOG_TAG, "resetHistory().");
+        LOGGER.info("resetHistory().");
         balanceRepository.reset();
     }
 
-    private void resetGeschiedenis(int periodeNummer) {
-        Log.i(LOG_TAG, "resetGeschiedenis() voor periode: " + periodeNummer);
-        SharedPreferences.Editor prefEditor = this.monitorPrefs.edit();
-        prefEditor.putInt(PERIODE_NUMMER, periodeNummer);
-        prefEditor.commit();
-        resetHistory();
-    }
-
-    class UsagePoint {
+    public class UsagePoint {
         private int dagInPeriode;
         private int balance;
         private int used;
