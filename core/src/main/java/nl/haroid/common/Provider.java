f2ad1cf3e2eb430a45d94f4c75f9c1184acd5c72
biker.ruud@gmail.com <biker-ruud@users.noreply.github.com>
2014-07-19 11:05:50 +0000

    Provider enum now has display names.

--- core/src/main/java/nl/haroid/common/Provider.java
+++ core/src/main/java/nl/haroid/common/Provider.java
@@ -5,7 +5,17 @@ package nl.haroid.common;
  */
 public enum Provider {
     // TODO (Xilv3r): add string variable so menu options look cleaner
-    HOLLANDS_NIEUWE,
-    HOLLANDS_NIEUWE_ZAKELIJK,
-    T_MOBILE
+    HOLLANDS_NIEUWE("Hollands Nieuwe"),
+    HOLLANDS_NIEUWE_ZAKELIJK("Hollands Nieuwe Zakelijk"),
+    T_MOBILE("T-Mobile");
+
+    private final String displayName;
+
+    private Provider(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
 }
